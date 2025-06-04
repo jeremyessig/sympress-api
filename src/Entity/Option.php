b01e5327fa9bf36baeca932bb67c8d1525d46b77
Jeremy <jeremy.essig@gmail.com>
2025-06-04 16:12:36 +0200

    Ajout des options

---
+++ src/Entity/Option.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\OptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ApiResource(
+    security: "is_granted('ROLE_ADMIN')",
+    normalizationContext: ['groups' => ['option:read', 'option:write']]
+)]
+#[Get()]
+#[Put()]
+#[GetCollection()]
+#[Post()]
+#[Patch()]
+#[Delete()]
+#[ORM\Entity(repositoryClass: OptionRepository::class)]
+#[ORM\Table(name: '`option`')]
+class Option
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private array $data = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
